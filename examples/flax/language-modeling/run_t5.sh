f356b8d904bc7fbcc22c18e00185453bf9ebb580
Hongshan Li <lihongshan8128@gmail.com>
2022-08-27 21:44:13 -0700

    wip play with t5

---
+++ examples/flax/language-modeling/run_t5.sh
@@ -0,0 +1,19 @@
+python run_t5_mlm_flax.py \
+	--output_dir="/workdir/norwegian-t5-base" \
+	--model_type="t5" \
+	--config_name="/workdir/norwegian-t5-base" \
+	--tokenizer_name="/workdir/norwegian-t5-base" \
+	--dataset_name="oscar" \
+    --cache_dir="/workdir/.cache/huggingface/datasets" \
+	--dataset_config_name="unshuffled_deduplicated_no" \
+	--max_seq_length="512" \
+	--per_device_train_batch_size="32" \
+	--per_device_eval_batch_size="32" \
+	--adafactor \
+	--learning_rate="0.005" \
+	--weight_decay="0.001" \
+	--warmup_steps="2000" \
+	--overwrite_output_dir \
+	--logging_steps="500" \
+	--save_steps="10000" \
+	--eval_steps="2500"
